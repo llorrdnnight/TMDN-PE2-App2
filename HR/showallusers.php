b4ece3bf36e9b39295684ac7db674144d629273e
Yannikaze <79192649+Yannikaze@users.noreply.github.com>
2021-03-10 08:52:03 +0100

    Update showallusers.php

--- HR/showallusers.php
+++ HR/showallusers.php
@@ -20,7 +20,7 @@ echo"<div><table class='usertable'><th colspan=8>Employees</th>
       <tr id='fix'><td><b>Id</b></td><td><b>Name</b></td><td><b>Lastname</b></td><td><b>Email</b></td><td><b>Phone</b><td><b>Salary</b></td><td><b>birthDate</b></td><td><b>Manage</b></td><tr>";
 while($row = $result->fetch_assoc()){
 
-echo"<tr><td>".$row['id']."</td><a href=delete.php?employee_id=".$row['id']."<td>".$row['firstName']."</td></a><td>".$row['lastName']."</td><td>".$row['mailAddress']."</td><td>".$row['phoneNumber']."</td><td>".$row['salary']."</td><td>".$row['birthDate']."</td><td><a href=delete.php?delete=".$row['id']."><img class='resize' src='../images/delete.png'></a><a href=manage.php?edit=".$row['id']."> <img class='resize' src='../images/manage.png'></a></td></tr>";
+echo"<tr><td>".$row['id']."</td><a href=employee_info.php?employee_id=".$row['id']."<td>".$row['firstName']."</td></a><td>".$row['lastName']."</td><td>".$row['mailAddress']."</td><td>".$row['phoneNumber']."</td><td>".$row['salary']."</td><td>".$row['birthDate']."</td><td><a href=delete.php?delete=".$row['id']."><img class='resize' src='../images/delete.png'></a><a href=manage.php?edit=".$row['id']."> <img class='resize' src='../images/manage.png'></a></td></tr>";
 
 }
 echo"</table></div>";
