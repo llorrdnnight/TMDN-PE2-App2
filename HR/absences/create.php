6fd8095dce22bc9ff95215bbf038bcf0bb8dac41
Gregori De Vuyst <greg.de.vuyst@gmail.com>
2021-03-24 12:40:56 +0100

    finalised javascript validation and helper functionalities

--- HR/absences/create.php
+++ HR/absences/create.php
@@ -50,11 +50,14 @@ else{
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/absences.css">
     <link rel="stylesheet" href="../css/state-messages.css">
-    <title>Document</title>
+    <title>Dashboard - File an absence</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../js/notification.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
 </head>
 <body>
+
+
 
     <div class="wrapper">
         <?php if($success): ?>
@@ -69,22 +72,36 @@ else{
         </div>
         <?php endif; ?>
         <div class="nav-bar">
-            <h1>File an absence</h1>
+            <div class="left">
+                <a href="../dashboard.php"><i class="fas fa-arrow-left"></i></a>
+                <h1>File an absence</h1>
+            </div>
+            <div class="right">
+            <i class="fa fa-user-circle"></i>
+            <span>John Doe</span>
+            </div>
+            
         </div>
         <div class="absence-container">
             <form action="" method="POST">
-
+                <div class="rt">
+                    <p class="rt-error"></p>
+                </div>                
                 <div class="row">
                     <label for="">Start date</label>
-                    <input type="date" name="startDate" id="">
+                    <input type="date" name="startDate" value="<?php echo date("Y-m-d") ?>" id="start">
+                </div>
+                <div class="row">
+                    <label for="">Days of absence</label>
+                    <input type="text" value="1" name="" id="days">
                 </div>
                 <div class="row">
                     <label for="">End date</label>
-                    <input type="date" name="endDate" id="">
+                    <input type="date" name="endDate" id="end">
                 </div>
                 <div class="row">
                     <label for="">Reason of absence</label>
-                    <textarea name="reason" id="" cols="30" rows="10"></textarea>
+                    <textarea name="reason" id="" cols="30" rows="10" spellcheck="false"></textarea>
                 </div>
 
                 <div class="row">
@@ -102,6 +119,133 @@ else{
     </div>
 
 
-    
+<script defer>
+
+
+
+// show no error message at beginning
+$(".rt").fadeOut(0);
+// setup variables
+startDate =  new Date($("#start").val());
+endDate = new Date();
+
+// set endDate = startDate + 1 @ startup
+endDate.setDate(startDate.getDate() +  parseInt($("#days").val()));
+$("#end").val(toStringDate(endDate));
+
+// modifies a data object to a string
+function toStringDate(date){
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    if(day < 10){
+        day = "0"+day;
+    }
+    if(month < 10){
+        month = "0"+month;
+    }
+    date = ""+year+"-"+month+"-"+day+"";
+    return date;
+}
+
+
+// shows an error message
+function showErrorMessage(selector, msg, time){
+
+    $(".rt-error").text(msg);
+    $(selector).fadeIn(500);
+    setTimeout(() => {
+        $(selector).fadeOut(500);
+    }, time);
+}
+
+var daysAbsence = $("#days");
+
+$("#start").change(() => {
+    // start vars
+    let startDate =  new Date($("#start").val());
+    let endDate = new Date($("#end").val());
+    let today = new Date();
+    // start date < today -> show error
+    if (startDate < today){
+        $("#start").val(toStringDate(today));
+        $(".rt-error").text("You cannot file an absence in the past!")
+        showErrorMessage(".rt", "You cannot file an absence in the past!", 3000);
+    }
+    // find the difference in days between 2 dates
+    const diffTime = Math.abs(endDate - startDate);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    // if diffdays is not a number - put empty string in days of absence
+    if(isNaN(diffDays)){
+    $("#days").val("");
+    }
+    // else put the calculated value
+    else{
+        $("#days").val(diffDays);
+    }
+    // if start and end date are equal -> show message -> adjust endDate so endDate = startDate + 1 day
+    if (endDate.getDay() == startDate.getDay() && endDate.getMonth() == startDate.getMonth() && endDate.getFullYear() == startDate.getFullYear() ){
+    console.log("they are equal");
+    endDate.setDate(startDate.getDate() +  1);
+    $("#days").val(1);
+    $("#end").val(toStringDate(endDate));
+    showErrorMessage(".rt", "You absence cannot last 0 days", 3000);
+}
+
+});
+
+
+// if days change
+$("#days").change(() => {
+
+// calculate end date
+startDate =  new Date($("#start").val());
+endDate = new Date();
+endDate.setDate(startDate.getDate() +  parseInt($("#days").val()));
+// adjust endate input
+$("#end").val(toStringDate(endDate));
+
+});
+
+$("#end").change(() => {
+// get dates
+let startDate =  new Date($("#start").val());
+let endDate = new Date($("#end").val());
+
+// end date cannot be < then start date
+if (endDate < startDate){
+    // if case -> endDate = startDate + 1 -> show error
+    endDate.setDate(startDate.getDate() +  1);
+    $("#end").val(toStringDate(endDate));
+    showErrorMessage(".rt", "You cannot file an absence in the past!", 3000);
+}
+
+// if start and end date are equal -> show message -> adjust endDate so endDate = startDate + 1 day
+if (endDate.getDay() == startDate.getDay() && endDate.getMonth() == startDate.getMonth() && endDate.getFullYear() == startDate.getFullYear() ){
+    console.log("they are equal");
+    endDate.setDate(startDate.getDate() +  1);
+    $("#days").val(1);
+    $("#end").val(toStringDate(endDate));
+    showErrorMessage(".rt", "You absence cannot last 0 days", 3000);
+}
+
+// calculate difference in days between the two dates
+const diffTime = Math.abs(startDate - endDate);
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+
+// adjust days input
+if(isNaN(diffDays)){
+    $("#days").val("");
+}
+else{
+    $("#days").val(diffDays);
+}
+});
+
+</script>
+
+
 </body>
 </html>
