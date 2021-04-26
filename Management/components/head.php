2e4fe05be432270107ee0cc6779a99e172fc001e
Jari Wilms <r0760106@student.thomasmore.be>
2021-04-26 22:21:24 +0200

    Duplicate code removal and a lot of refactoring
    like a lot.
    Added PATHS file for easier access of files in our pages.
    I have talked with Lukas from App1 in order to 'sync' our css somewhat so our pages look the same.
    Ik hoop voor het einde van dit semester een werkende login te krijgen zodat ik dat niet altijd weg moet commenten.

--- Management/components/head.php
+++ Management/components/head.php
@@ -1,11 +1,14 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"] . "/TMDN-PE2-App2/PATHS.PHP"); ?>
+
 <!DOCTYPE html> <!-- Head component for default scripts and links -->
 <html lang="en">
 <head>
     <meta charset="UTF-8"><!-- Default META tags -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/TMDN-PE2-App2/Management/style/reset.css">
+    <link rel="icon" href=<?= HTMLIMAGES . "LOGO_origin.png" ?>>
+    <link rel="stylesheet" href=<?= HTMLCSS . CSS_RESET ?>>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/TMDN-PE2-App2/Management/style/style.css"><!-- Default stylesheet, TODO: merge with Thomas' stylesheet -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><!-- Jquery -->
-    <script src="/TMDN-PE2-App2/Management/scripts/javascript/collapseListitems.js"></script><!-- For collapsing navigation <li> items -->
+    <link rel="stylesheet" href=<?= HTMLCSS . CSS_STYLE ?>>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src=<?= HTMLJAVASCRIPT . "collapseListItems.js"; ?>></script>
---
+++ TEMPLATE.php
@@ -0,0 +1,14 @@
+<?php
+    require($_SERVER["DOCUMENT_ROOT"] . "/PATHS.php");
+?>
+
+<?php require(COMPONENTSDIR . COMPONENT_HEAD); ?>
+    <!-- Add non-standard scripts and links here -->
+    <title><!-- Page title --></title>
+</head>
+<?php require(COMPONENTSDIR . COMPONENT_BODY_TOP); ?>
+
+    <!-- Header title can be set in the body_top.php page => components/body_top.php -->
+    <!-- Insert content here -->
+
+<?php require(COMPONENTSDIR . COMPONENT_BODY_BOTTOM); ?>
